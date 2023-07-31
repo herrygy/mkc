e9b6e9e427ff48758988e1823a44ea74f07a0c3f
djl123 <921321775@qq.com>
2023-07-31 14:25:23 +0800

    vercel 跨域

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,19 @@
 // 该服务为 vercel serve跨域处理
-const { createProxyMiddleware } = require('http-proxy-middleware')
+import createProxyMiddleware from 'http-proxy-middleware'
+// const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
   let target = ''
 
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('/api')) {
+  if (req.url.startsWith('/pay-management')) {
     // 这里填目标地址
     target = 'http://47.108.163.219:10070'
   }
 
-  console.log("url:",req.url)
-  console.log("target:",target)
+  console.log('url:', req.url)
+  console.log('target:', target)
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
@@ -20,7 +21,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/api`
       // 例如 /api/user/login 将被转发到 http://target/user/login
-      '^/api/': '/'
+      '^/pay-management/': '/pay-management'
     }
   })(req, res)
 }
