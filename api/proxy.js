80179e45e9377adeff8915cd55c500e19a578702
djl123 <921321775@qq.com>
2023-08-01 22:41:00 +0800

    update vercel跨域

--- api/proxy.js
+++ api/proxy.js
@@ -1,27 +1,27 @@
-// 该服务为 vercel serve跨域处理
-// import createProxyMiddleware from 'http-proxy-middleware'
-const { createProxyMiddleware } = require('http-proxy-middleware')
-
-module.exports = (req, res) => {
-  let target = ''
-
-  // 代理目标地址
-  // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('/pay-management')) {
-    // 这里填目标地址
-    target = 'http://47.108.163.219:10070'
-  }
-
-  console.log('url:', req.url)
-  console.log('target:', target)
-  // 创建代理对象并转发请求
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/api`
-      // 例如 /api/user/login 将被转发到 http://target/user/login
-      '^/pay-management/': '/pay-management/'
-    }
-  })(req, res)
-}
+// // 该服务为 vercel serve跨域处理
+// // import createProxyMiddleware from 'http-proxy-middleware'
+// const { createProxyMiddleware } = require('http-proxy-middleware')
+//
+// module.exports = (req, res) => {
+//   let target = ''
+//
+//   // 代理目标地址
+//   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
+//   if (req.url.startsWith('/pay-management')) {
+//     // 这里填目标地址
+//     target = 'http://47.108.163.219:10070'
+//   }
+//
+//   console.log('url:', req.url)
+//   console.log('target:', target)
+//   // 创建代理对象并转发请求
+//   createProxyMiddleware({
+//     target,
+//     changeOrigin: true,
+//     pathRewrite: {
+//       // 通过路径重写，去除请求路径中的 `/api`
+//       // 例如 /api/user/login 将被转发到 http://target/user/login
+//       '^/pay-management/': '/pay-management/'
+//     }
+//   })(req, res)
+// }
